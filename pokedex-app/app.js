f2d4c3874fa4ecae832724f313d9540d292f91a9
Chris Bellaro <chrisbellaro@Chriss-Air.home>
2020-02-09 22:52:30 -0500

    API up and running

--- pokedex-app/app.js
+++ pokedex-app/app.js
@@ -1,4 +1,18 @@
 $(() => {
-    console.log($);
-    console.log('test');
+    $('form').on('submit', (event)=>{
+        event.preventDefault();
+        let userInput = $('input[type="number"]').val()
+        $.ajax({
+            url:'https://pokeapi.co/api/v2/pokemon-species/' + userInput +'/'
+        }).then(
+            (data)=>{
+                $('#title').html(data.Title);
+                $('#year').html(data.Year);
+                $('#rated').html(data.Rated);
+            },
+            () => {
+                console.log('bad request');
+            }
+        );
+    })
 })
