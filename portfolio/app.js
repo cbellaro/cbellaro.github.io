9015a40709007076994640b8125e22730b87f378
Chris Bellaro <chrisbellaro@Chriss-Air.home>
2020-04-09 12:26:04 -0400

    added more styling, updated scanline animations, updated portfolio links and jquery

--- portfolio/app.js
+++ portfolio/app.js
@@ -8,17 +8,25 @@ $(() => {
       window.open("https://cbellaro-express-mongo-example.herokuapp.com/")
     } else if (userInput === "run movie_critiq.exe" || userInput === "run movie critiq.exe" || userInput === "run movie-critiq.exe"){
       window.open("https://movie-critique.herokuapp.com/")
-    } else if (userInput === "run final_project.exe"){
-      window.open("#")
+    } else if (userInput === "run doodle_duel.exe" || userInput === "run doodle duel.exe" || userInput === "run doodle-duel.exe"){
+      alert("Coming soon")
     } else if (userInput === "run resume.exe"){
       window.open("https://docs.google.com/document/d/1Xz9ZTBsB3tinqpnvqRSaOQi0Kq3QyNW83PWwMh8IN7k/")
-    } else if (userInput === "run bio.exe"){
-      window.open("https://profiles.generalassemb.ly/profiles/16665/preview")
+    } else if (userInput === "run general_assembly_profile.exe"){
+      alert("Coming soon")
     } else if (userInput === "run github.exe"){
       window.open("https://github.com/cbellaro/")
     } else if (userInput === "run linkedin.exe"){
       window.open("https://www.linkedin.com/in/chris-bellaro/")
     } else (alert("Error. Please input a listed command."))
+  })
+
+  $('#doodle-duel').on('click', (event)=>{
+    alert("Coming soon")
+  })
+
+  $('#ga-profile').on('click', (event)=>{
+    alert("Coming soon")
   })
 
   $('form').on('click', (event)=>{
