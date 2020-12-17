dfe79a748a9aaa5899ab28f87887ddb5be2434d7
Chris Bellaro <chrisbellaro@Chriss-Air.home>
2020-12-16 19:28:47 -0500

    edited some links

--- portfolio/app.js
+++ portfolio/app.js
@@ -4,16 +4,16 @@ $(() => {
     userInput = $('input[type="text"]').val()
     if (userInput === "run functional_pokedex.exe" || userInput === "run functional pokedex.exe" || userInput === "run functional-pokedex.exe" ){
       window.open("https://cbellaro.github.io/pokedex-app/");
-    } else if (userInput === "run bookly.exe"){
-      window.open("https://cbellaro-express-mongo-example.herokuapp.com/")
+    } else if (userInput === "run sneakfreaks.exe"){
+      window.open("https://sneakfreaks-frontend.herokuapp.com/")
     } else if (userInput === "run movie_critiq.exe" || userInput === "run movie critiq.exe" || userInput === "run movie-critiq.exe"){
       window.open("https://movie-critique.herokuapp.com/")
     } else if (userInput === "run doodle_duel.exe" || userInput === "run doodle duel.exe" || userInput === "run doodle-duel.exe"){
       window.open("https://doodle-duel-frontend.herokuapp.com/")
     } else if (userInput === "run resume.exe"){
       window.open("https://docs.google.com/document/d/1ROvZV0goACfaDSJS7d_4RiFPWkCPN-nCa3Z1t0HBAvo")
-    } else if (userInput === "run general_assembly_profile.exe"){
-      window.open("https://profiles.generalassemb.ly/profiles/chris-bellaro")
+    } else if (userInput === "run blog.exe"){
+      window.open("https://medium.com/@cbellaro")
     } else if (userInput === "run github.exe"){
       window.open("https://github.com/cbellaro/")
     } else if (userInput === "run linkedin.exe"){
